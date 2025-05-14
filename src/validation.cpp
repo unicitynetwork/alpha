16d2de26acc276f92839a4ccde35f2af4af93964
sakuyama2024 <makoto.sakuyama@proton.me>
2025-05-14 20:46:41 +0400

    updated timebomb to 300,000

--- src/validation.cpp
+++ src/validation.cpp
@@ -3185,8 +3185,8 @@ bool Chainstate::ActivateBestChainStep(BlockValidationState& state, CBlockIndex*
     int nHeight = pindexFork ? pindexFork->nHeight : -1;
     
     //Shut down to force recompilation
-      if (nHeight == 250000)
-          return FatalError(m_chainman.GetNotifications(), state, "Forced shutdown at block 250,000. Get latest version");
+      if (nHeight == 300000)
+          return FatalError(m_chainman.GetNotifications(), state, "Forced shutdown at block 300,000. Get latest version");
 
     while (fContinue && nHeight != pindexMostWork->nHeight) {
         // Don't iterate the entire list of potential improvements toward the best tip, as we likely only need
